bfc306c862de124a826e655299fa95946998eb10
dervvy <xtqzzzz@yandex.ru>
2024-05-22 23:20:35 +0300

    new

--- lekar/settings.py
+++ lekar/settings.py
@@ -116,12 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = '/path/to/your/static/root'
-
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 MEDIA_URL = "/media/"
